33940148a2fe627fef0d557c0bbfd470894a8cff
NikitaPetrovsiy <nikpetrovskiy23@gmail.com>
2019-11-05 13:54:41 +0300

    Created new actionCreators for control 'loading' indicator

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 
 import BookListItem from '../book-list-item';
 import { withBookstoreService } from '../hoc';
-import { booksLoaded } from '../../actions';
+import { booksLoaded, booksRequested } from '../../actions';
 import { compose } from "../../utils";
 import Spinner from "../spinner";
 
@@ -12,7 +12,8 @@ import './book-list.css';
 class BookList extends Component {
 
     componentDidMount() {
-        const { bookstoreService, booksLoaded } = this.props;
+        const { bookstoreService, booksLoaded, booksRequested } = this.props;
+        booksRequested();
         bookstoreService.getBooks()
             .then((data) => {
                 booksLoaded(data);
@@ -44,7 +45,7 @@ const mapStateToProps = ({ books, loading }) => {
       loading
   };
 };
-const mapDispatchToProps = { booksLoaded };
+const mapDispatchToProps = { booksLoaded, booksRequested };
 
 export default compose(
     withBookstoreService(),
